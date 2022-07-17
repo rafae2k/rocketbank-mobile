9354aa604889ba1b5ef80b34632f94fa8b6b8861
Rafael Feitoza <rafaeljr@outlook.com>
2022-07-17 18:40:57 -0300

    feat(App): load fonts and add statusbar

--- App.tsx
+++ App.tsx
@@ -1,56 +1,51 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { useFonts, Urbanist_400Regular, Urbanist_500Medium, Urbanist_700Bold } from '@expo-google-fonts/urbanist';
-import * as SplashScreen from 'expo-splash-screen';
-import { StyleSheet } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Routes from './src/routes';
-import { useCallback, useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import {
+  useFonts,
+  Urbanist_400Regular,
+  Urbanist_500Medium,
+  Urbanist_700Bold
+} from '@expo-google-fonts/urbanist'
+import * as SplashScreen from 'expo-splash-screen'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import Routes from './src/routes'
+import { useCallback, useEffect, useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
 
-void SplashScreen.preventAutoHideAsync();
+void SplashScreen.preventAutoHideAsync()
 
 export default function App() {
-
   const [isReady, setIsReady] = useState(false)
   const [fontsLoaded] = useFonts({
     Urbanist_500Medium,
     Urbanist_400Regular,
-    Urbanist_700Bold,
-  });
+    Urbanist_700Bold
+  })
 
-  const onFinish = useCallback( async () => {
-    await SplashScreen.preventAutoHideAsync();
-    setIsReady(true);
-  }, []);
+  const onFinish = useCallback(async () => {
+    await SplashScreen.preventAutoHideAsync()
+    setIsReady(true)
+  }, [])
 
   useEffect(() => {
     void onFinish()
-  }, [onFinish]);
+  }, [onFinish])
 
   const handleLoading = useCallback(async () => {
-    await SplashScreen.hideAsync();
-  }, []);
+    await SplashScreen.hideAsync()
+  }, [])
 
   useEffect(() => {
-    if (fontsLoaded && isReady) void handleLoading();
+    if (fontsLoaded && isReady) void handleLoading()
   }, [fontsLoaded, handleLoading, isReady])
-  
-  if (!isReady) return null;
 
+  if (!isReady) return null
 
   return (
     <NavigationContainer>
       <SafeAreaProvider>
         <Routes />
+        <StatusBar style="auto" />
       </SafeAreaProvider>
     </NavigationContainer>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
