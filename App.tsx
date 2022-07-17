288cabdf05064d4a6010b042931fe8cefdc04a9f
Rafael Feitoza <rafaeljr@outlook.com>
2022-07-17 11:28:52 -0300

    feat: add routes
    closed #5

--- App.tsx
+++ App.tsx
@@ -1,12 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { useFonts, Urbanist_400Regular, Urbanist_500Medium, Urbanist_700Bold } from '@expo-google-fonts/urbanist';
+import * as SplashScreen from 'expo-splash-screen';
+import { StyleSheet } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Routes from './src/routes';
+import { useCallback, useEffect, useState } from 'react';
+
+void SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const [isReady, setIsReady] = useState(false)
+  const [fontsLoaded] = useFonts({
+    Urbanist_500Medium,
+    Urbanist_400Regular,
+    Urbanist_700Bold,
+  });
+
+  const onFinish = useCallback( async () => {
+    await SplashScreen.preventAutoHideAsync();
+    setIsReady(true);
+  }, []);
+
+  useEffect(() => {
+    void onFinish()
+  }, [onFinish]);
+
+  const handleLoading = useCallback(async () => {
+    await SplashScreen.hideAsync();
+  }, []);
+
+  useEffect(() => {
+    if (fontsLoaded && isReady) void handleLoading();
+  }, [fontsLoaded, handleLoading, isReady])
+  
+  if (!isReady) return null;
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <Routes />
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 }
 
