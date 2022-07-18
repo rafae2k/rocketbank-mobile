8b55d9995f2c44f3882f1cc7b658954838ad2b3f
Rafael Feitoza <rafaeljr@outlook.com>
2022-07-18 12:56:31 -0300

    feat: add index import to `components` and `screens`

---
+++ src/screens/index.ts
@@ -0,0 +1,7 @@
+import Account from './Account'
+import Home from './Home'
+import Login from './Login'
+import Portfolio from './Portfolio'
+import Stocks from './Stocks'
+
+export { Account, Home, Login, Portfolio, Stocks }
