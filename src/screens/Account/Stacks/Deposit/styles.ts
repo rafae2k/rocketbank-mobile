68f354a3f76e4c3e0a75a51297bd564facfa69e8
Rafael Feitoza <rafaeljr@outlook.com>
2022-07-19 12:30:35 -0300

    feat(Account): add deposit screen
    closed #17

---
+++ src/screens/Account/Stacks/Deposit/styles.ts
@@ -0,0 +1,44 @@
+import styled, { css } from 'styled-components/native'
+
+export const Container = styled.View`
+  ${({ theme }) => css`
+    flex: 1;
+    padding: 30px ${theme.space.md} 0;
+  `}
+`
+
+export const LineWrapper = styled.View`
+  ${({ theme }) => css`
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: ${theme.space.md};
+  `}
+`
+
+export const QrCodeWrapper = styled.View`
+  ${({ theme }) => css`
+    align-items: center;
+    margin-bottom: ${theme.space.xl};
+  `}
+`
+
+export const InfoWrapper = styled.View`
+  ${({ theme }) => css`
+    margin-bottom: ${theme.space.xl};
+  `}
+`
+
+export const TextRight = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.fontFamily};
+    font-size: ${theme.fontSize.base};
+  `}
+`
+
+export const TextLeft = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.fontFamily};
+    font-size: ${theme.fontSize.md};
+    font-weight: ${theme.fontWeight.bold};
+  `}
+`
