b861e8f009986d3cd6d1c50c11df10458b001940
Rafael Feitoza <rafaeljr@outlook.com>
2022-07-20 09:16:43 -0300

    feat: add `TabIcon` component

---
+++ src/components/TabIcon/index.tsx
@@ -0,0 +1,62 @@
+import { Feather } from '@expo/vector-icons'
+import { StyleSheet, Text, View } from 'react-native'
+import theme from '../../styles/theme'
+
+type TabBarIconProps = {
+  focused: boolean
+  icon: React.ComponentProps<typeof Feather>['name']
+  label: string
+}
+
+export default function TabIcon({ focused, icon, label }: TabBarIconProps) {
+  return (
+    <View style={styles.container}>
+      {!focused && <View style={styles.dot} />}
+
+      {focused && (
+        <View
+          style={[
+            styles.dot,
+            focused
+              ? { backgroundColor: theme.colors.primary.brand }
+              : { backgroundColor: theme.colors.neutral.gray }
+          ]}
+        />
+      )}
+      <Feather
+        name={icon}
+        size={20}
+        color={focused ? theme.colors.primary.brand : theme.colors.neutral.gray}
+        style={styles.icon}
+      />
+      <Text style={focused ? styles.focused : styles.label}>{label}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative'
+  },
+  dot: {
+    top: 0,
+    left: 0,
+    width: 8,
+    height: 6,
+    borderRadius: 999,
+    marginBottom: 4
+  },
+  icon: {
+    marginBottom: 4
+  },
+  label: {
+    color: theme.colors.neutral.gray,
+    fontFamily: theme.fontWeight.light
+  },
+  focused: {
+    color: theme.colors.primary.brand,
+    fontFamily: theme.fontWeight.medium
+  }
+})
