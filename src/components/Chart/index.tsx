057414ce518e081543ab17d68e13bf91ce174ed6
Rafael Feitoza <rafaeljr@outlook.com>
2022-07-20 11:15:57 -0300

    feat(`components`): add `Chart`component

---
+++ src/components/Chart/index.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react'
+import { Text } from 'react-native'
+
+import { Container } from './styles'
+
+export default function Chart() {
+  return (
+    <Container>
+      <Text>Chart</Text>
+    </Container>
+  )
+}
