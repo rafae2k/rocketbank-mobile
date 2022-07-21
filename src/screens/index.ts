1c1ac9a846fa42f262e8bbd92adb5d2ba925ac65
Rafael Feitoza <rafaeljr@outlook.com>
2022-07-21 09:03:11 -0300

    feat(`Screens`): add `Stocks` screen
    closed #13

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -3,5 +3,6 @@ import Home from './Home'
 import Login from './Login'
 import Portfolio from './Portfolio'
 import Stocks from './Stocks'
+import StockDetails from './Stocks/StockDetails'
 
-export { Deposit, Withdraw, Home, Login, Portfolio, Stocks }
+export { Deposit, Withdraw, Home, Login, Portfolio, Stocks, StockDetails }
