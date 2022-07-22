5c6b5a8edb382f2fe6ffec44234bfb9f82219e41
Rafael Feitoza <rafaeljr@outlook.com>
2022-07-22 11:45:37 -0300

    feat(`Screens`): add `Buy` screen

--- src/screens/BuyOrSell/Buy/index.tsx
+++ src/screens/BuyOrSell/Buy/index.tsx
@@ -1,17 +1,142 @@
-import { ReactNode } from 'react'
-import { Text } from 'react-native'
+import { useState } from 'react'
+import { Keyboard, TouchableWithoutFeedback } from 'react-native'
+import RNPickerSelects from 'react-native-picker-select'
 
-import { Container } from './styles'
+import * as S from './styles'
+import CurrencyInput from 'react-native-currency-input'
+import theme from '../../../styles/theme'
+import { Button } from '../../../components'
+import { formatToCurrency } from '../../../utils/currencyFormatter'
+import { BalanceInfo } from '../../../components/BalanceInfo'
 
-interface BuyProps {
-  children: ReactNode
-}
+export default function Buy() {
+  const STOCK_PRICE = 10
+  const BALANCE = 100
 
-export default function Buy({ children }: BuyProps) {
+  const [state, setState] = useState({
+    quantity: 0,
+    total: 0,
+    buyerPower: BALANCE - STOCK_PRICE,
+    isAboveLimit: BALANCE < STOCK_PRICE
+  })
+
+  const [, setOrderType] = useState('')
+  const [orderFocus, setOrderFocus] = useState(false)
+
+  const handleChangeQuantity = (value: number) => {
+    setState({
+      ...state,
+      quantity: value,
+      total: value * STOCK_PRICE,
+      buyerPower: BALANCE - value * STOCK_PRICE,
+      isAboveLimit: BALANCE < value * STOCK_PRICE
+    })
+  }
+
+  const totalFormatted = formatToCurrency(state.total, 'pt-BR', 'BRL')
+  const priceFormatted = formatToCurrency(STOCK_PRICE, 'pt-BR', 'BRL')
+  const balanceFormatted = formatToCurrency(state.buyerPower, 'pt-BR', 'BRL')
+  const stockPriceFormatted = formatToCurrency(STOCK_PRICE, 'pt-BR', 'BRL')
+
   return (
-    <Container>
-      <Text>Buy</Text>
-      {children}
-    </Container>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <S.Container>
+        <S.TopContainer>
+          <S.ItemWrapper>
+            <S.Right>Tipo de ordem</S.Right>
+            <RNPickerSelects
+              onValueChange={(value: string) => setOrderType(value)}
+              items={[
+                { label: 'Limitada', value: 'limitada' },
+                { label: 'a mercado', value: 'a-mercado' },
+                { label: 'Stop Loss', value: 'hockey' }
+              ]}
+              placeholder={{
+                label: 'Selecione o tipo de ordem'
+              }}
+              onOpen={() => setOrderFocus(!orderFocus)}
+              onClose={() => setOrderFocus(!orderFocus)}
+              style={{
+                placeholder: {
+                  color: orderFocus
+                    ? theme.colors.primary.brand
+                    : theme.colors.neutral.gray,
+                  fontSize: 14,
+                  fontFamily: theme.fontWeight.medium
+                },
+                viewContainer: {
+                  borderWidth: 1,
+                  borderColor: orderFocus
+                    ? theme.colors.primary.brand
+                    : theme.colors.neutral.gray,
+                  padding: 8,
+                  borderRadius: 4
+                }
+              }}
+            />
+          </S.ItemWrapper>
+
+          <S.ItemWrapper>
+            <S.Right>Preço Atual</S.Right>
+            <S.Left>{priceFormatted}</S.Left>
+          </S.ItemWrapper>
+
+          <S.ItemWrapper>
+            <S.Right>Quantidade</S.Right>
+            <S.InputWrapper>
+              <CurrencyInput
+                placeholder="100"
+                keyboardType="numeric"
+                prefix=""
+                delimiter="."
+                separator=""
+                precision={0}
+                minValue={0}
+                value={state.quantity}
+                // @ts-ignore
+                onChangeValue={(value) => handleChangeQuantity(value)}
+                style={{
+                  fontSize: 22
+                }}
+              />
+              <S.Line />
+            </S.InputWrapper>
+          </S.ItemWrapper>
+
+          <BalanceInfo
+            title="Poder de Compra"
+            balanceFormatted={balanceFormatted}
+            isAboveLimit={state.isAboveLimit}
+          />
+        </S.TopContainer>
+
+        <S.CenterHeader>Detalhes</S.CenterHeader>
+        <S.RecipeContainer>
+          <S.ItemWrapper>
+            <S.Right>Taxas</S.Right>
+            <S.Right>R$ 12,99</S.Right>
+          </S.ItemWrapper>
+
+          <S.ItemWrapper>
+            <S.Right>Preço de compra</S.Right>
+            <S.Right>{stockPriceFormatted}</S.Right>
+          </S.ItemWrapper>
+
+          <S.ItemWrapper>
+            <S.Right>Quantidade</S.Right>
+            <S.Right>{state.quantity}</S.Right>
+          </S.ItemWrapper>
+
+          <S.TotalWrapper>
+            <S.TotalHeader>Total</S.TotalHeader>
+            <S.TotalAmount>{totalFormatted}</S.TotalAmount>
+          </S.TotalWrapper>
+        </S.RecipeContainer>
+
+        <S.ButtonWrapper>
+          <Button label="Comprar" disabled={state.isAboveLimit} />
+        </S.ButtonWrapper>
+      </S.Container>
+    </TouchableWithoutFeedback>
   )
 }
--- src/screens/BuyOrSell/Buy/styles.ts
+++ src/screens/BuyOrSell/Buy/styles.ts
@@ -1,5 +1,111 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native'
+import CurrencyInput from 'react-native-currency-input'
+
 
 export const Container = styled.View`
+  ${({ theme }) => css`
+    flex: 1;
+    background-color: ${theme.colors.background};
+    padding: ${theme.space.xl} ${theme.space.md} 0;
+  `}
+`
 
-`;
+export const TopContainer = styled.View`
+  ${() => css``}
+`
+
+export const ItemWrapper = styled.View`
+  ${({ theme }) => css`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: ${theme.space.md};
+  `}
+`
+
+export const Right = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.fontSize.md};
+    font-family: ${theme.fontWeight.light};
+    color: ${theme.colors.text.onBackground};
+  `}
+`
+
+export const Left = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.fontSize.lg};
+    font-family: ${theme.fontWeight.bold};
+  `}
+`
+
+export const InputWrapper = styled.View`
+  ${({ theme }) => css`
+    height: ${theme.space.xxl};
+    padding: ${theme.space.base};
+    justify-content: center;
+    align-items: center;
+  `}
+`
+
+export const Line = styled.View`
+  ${({ theme }) => css`
+    height: 2px;
+    min-width: 100px;
+    background-color: ${theme.colors.neutral.black};
+    justify-content: center;
+    margin-top: ${theme.space.base};
+  `}
+`
+
+export const Input = styled(CurrencyInput)`
+  ${({ theme }) => css`
+    width: 200px;
+    font-size: ${theme.fontSize.xxl};
+    font-family: ${theme.fontWeight.bold};
+  `}
+`
+
+export const RecipeContainer = styled.View`
+  ${({ theme }) => css`
+    border: 1px solid ${theme.colors.neutral.grayLight};
+    border-radius: ${theme.space.base};
+    padding: ${theme.space.md};
+    margin-bottom: ${theme.space.md};
+  `}
+`
+
+export const CenterHeader = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.fontSize.md};
+    font-family: ${theme.fontWeight.bold};
+    text-align: center;
+    margin-bottom: ${theme.space.lg};
+  `}
+`
+
+export const TotalWrapper = styled.View`
+  ${() => css`
+    align-items: flex-end;
+  `}
+`
+
+export const TotalHeader = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.fontSize.base};
+    font-family: ${theme.fontWeight.light};
+    margin-bottom: ${theme.space.sm};
+  `}
+`
+
+export const TotalAmount = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.fontSize.lg};
+    font-family: ${theme.fontWeight.bold};
+  `}
+`
+
+export const ButtonWrapper = styled.View`
+  ${({ theme }) => css`
+    height: ${theme.space.xxl};
+  `}
+`
